5a7cccf5057871e219be5334618adb6ce9ffa2ee
dm18 <dm18@users.noreply.github.com>
2024-01-04 23:53:40 -0800

    f ix call

--- webui-user-loop.bat
+++ webui-user-loop.bat
@@ -10,9 +10,11 @@ set VENV_DIR=
 set COMMANDLINE_ARGS=--autolaunch --update-check --xformers --api --theme dark
 set XFORMERS_PACKAGE=xformers==0.0.20
 
+cd ..
+cd ..
 
 :start
 echo "loop Start"
-call ..\..\webui.bat < batch.log
+call webui.bat < batch.log
 echo "looped"
 goto start
